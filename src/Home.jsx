3cad66e3f51caea71e603d7c89622f44e157924f
hollymsnell <hollymsnell@gmail.com>
2023-10-12 13:48:47 -0600

    updating index on frontend following steps in react guide

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,23 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { PhotosIndex } from "./PhotosIndex";
 
   export function Home() {
+    const [photos, setPhotos] = useState([]);
+
+       const handleIndexPhotos = () => {
+         console.log("handleIndexPhotos");
+         axios.get("http://localhost:3000/photos.json").then((response) => {
+           console.log(response.data);
+           setPhotos(response.data);
+         });
+       };
+    
+       useEffect(handleIndexPhotos, []);
+
     return (
       <div>
-       <PhotosIndex />
+       <PhotosIndex photos={photos} />
       </div>
     );
   }
