363a60c92a2320375cee0000f2c1c4bd06c4b08b
hollymsnell <hollymsnell@gmail.com>
2023-10-17 10:42:16 -0600

    handle the form submit to run the props update function

--- src/PhotosShow.jsx
+++ src/PhotosShow.jsx
@@ -1,4 +1,11 @@
 export function PhotosShow(props) {
+
+  const handleSubmit = (event) => {
+        event.preventDefault();
+        const params = new FormData(event.target);
+         props.onUpdatePhoto(props.photo.id, params, () => event.target.reset());
+       };
+
   return (
     <div>
       <h1>Photo information</h1>
@@ -6,7 +13,7 @@ export function PhotosShow(props) {
       <p>Location: {props.photo.location}</p>
       <p>Width: {props.photo.width}</p>
       <p>Height: {props.photo.height}</p>
-      <form>
+      <form onSubmit={handleSubmit}>
          <div>
            Name: <input defaultValue="props.photo.name" name="name" type="text" />
          </div>
